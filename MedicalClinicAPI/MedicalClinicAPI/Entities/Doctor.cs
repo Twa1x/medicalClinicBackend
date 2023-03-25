fecdeb2d605628adf44ddc308891de1eba5e1281
Calcioiu Alexandru <77210765+Twa1x@users.noreply.github.com>
2023-03-25 16:39:20 +0200

    Created entities

---
+++ MedicalClinicAPI/MedicalClinicAPI/Entities/Doctor.cs
@@ -0,0 +1,13 @@
+﻿namespace MedicalClinicAPI.Entities
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string Specialisation { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public List<Pacient> Pacients { get; set; }
+
+    }
+}
---
+++ MedicalClinicAPI/MedicalClinicAPI/Entities/Pacient.cs
@@ -0,0 +1,14 @@
+﻿namespace MedicalClinicAPI.Entities
+{
+    public class Pacient
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+        public long SSN { get; set; }
+
+        public int DoctorId { get; set; }
+        public Doctor Doctor { get; set; }
+    }
+}
