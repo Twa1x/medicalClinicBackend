02d8f5b6f14d8516240374162ce16429424f262d
Calcioiu Alexandru <77210765+Twa1x@users.noreply.github.com>
2023-03-25 16:57:41 +0200

    Added DbContext

---
+++ MedicalClinicAPI/MedicalClinicAPI/DbContext.cs
@@ -0,0 +1,93 @@
+﻿using MedicalClinicAPI.Entities;
+
+namespace MedicalClinicAPI
+{
+    public class DbContext
+    {
+
+        public static List<Doctor> Doctors = new List<Doctor>
+        {
+            new Doctor
+            {
+                Id=1,
+                FirstName="Mihai",
+                LastName="Pop",
+                Specialisation="ORL",
+                Pacients = new List<Pacient>
+                {
+                   new Pacient
+                   {
+                       Id=1,
+                       FirstName="Alex",
+                       LastName="Ion",
+                       Age= 17,
+                       SSN=1232131321,
+                       DoctorId=1
+                   }
+                   ,
+                   new Pacient
+                   {
+                       Id=2,
+                       FirstName="Vasile",
+                       LastName="Mihai",
+                       Age= 35,
+                       SSN=5532131321,
+                       DoctorId=1
+                   }
+                   ,
+                   new Pacient
+                   {
+                       Id=3,
+                       FirstName="Catalin",
+                       LastName="Popa",
+                       Age= 19,
+                       SSN=6782131321,
+                       DoctorId=1
+                   }
+                }
+
+            }
+            ,
+             new Doctor
+            {
+                Id=2,
+                FirstName="Ionel",
+                LastName="Schiopu",
+                Specialisation="Ortoped",
+                Pacients = new List<Pacient>
+                {
+                   new Pacient
+                   {
+                       Id=4,
+                       FirstName="Marian",
+                       LastName="Popescu",
+                       Age= 53,
+                       SSN=9932131321,
+                       DoctorId=1
+                   }
+                   ,
+                   new Pacient
+                   {
+                       Id=5,
+                       FirstName="Catalina",
+                       LastName="Mirela",
+                       Age= 35,
+                       SSN=8632131321,
+                       DoctorId=1
+                   }
+                   ,
+                   new Pacient
+                   {
+                       Id=6,
+                       FirstName="Lili",
+                       LastName="Trandafir",
+                       Age= 25,
+                       SSN=6422131321,
+                       DoctorId=1
+                   }
+                }
+
+            }
+        };
+    }
+}
--- MedicalClinicAPI/MedicalClinicAPI/Entities/Pacient.cs
+++ MedicalClinicAPI/MedicalClinicAPI/Entities/Pacient.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Age { get; set; }
+        public int Age { get; set; }
         public long SSN { get; set; }
 
         public int DoctorId { get; set; }
