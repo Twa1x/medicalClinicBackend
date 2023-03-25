95051657cba77222d4a1cbc1b46f0f85772ade41
Calcioiu Alexandru <77210765+Twa1x@users.noreply.github.com>
2023-03-25 17:05:54 +0200

    Create DoctorRepository.cs

---
+++ MedicalClinicAPI/MedicalClinicAPI/Repositories/DoctorRepository.cs
@@ -0,0 +1,23 @@
+﻿using MedicalClinicAPI.Entities;
+
+namespace MedicalClinicAPI.Repositories
+{
+    public class DoctorRepository
+    {
+        public List<Doctor>GetAlL()
+        {
+            var result = DbContext.Doctors;
+
+            return result;
+        }
+
+        public Doctor GetById(int doctorId)
+        {
+            var result = DbContext.Doctors
+                .Where(e => e.Id == doctorId)
+                .FirstOrDefault();
+
+            return result;
+        }
+    }
+}
