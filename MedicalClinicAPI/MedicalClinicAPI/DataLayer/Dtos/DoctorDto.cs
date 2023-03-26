69b7af8963a611d5316af9d756d13d2473cb2206
Calcioiu Alexandru <77210765+Twa1x@users.noreply.github.com>
2023-03-26 13:22:58 +0300

    Created DTOS for pacient and doctor.

---
+++ MedicalClinicAPI/MedicalClinicAPI/DataLayer/Dtos/DoctorDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MedicalClinicAPI.DataLayer.Dtos
+{
+    public class DoctorDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Specialisation { get; set; }
+        public List<PacientDto> Pacients { get; set; }
+    }
+}
---
+++ MedicalClinicAPI/MedicalClinicAPI/DataLayer/Dtos/PacientDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MedicalClinicAPI.DataLayer.Dtos
+{
+    public class PacientDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
