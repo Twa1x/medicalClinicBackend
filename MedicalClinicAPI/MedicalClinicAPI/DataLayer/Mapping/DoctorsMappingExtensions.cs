8297b260fbd14c886acb886dfd8b9d333f1d8d15
Calcioiu Alexandru <77210765+Twa1x@users.noreply.github.com>
2023-03-26 13:30:00 +0300

    Pacients and Doctors Mapping created

--- MedicalClinicAPI/MedicalClinicAPI/DataLayer/Mapping/DoctorsMappingExtensions.cs
+++ MedicalClinicAPI/MedicalClinicAPI/DataLayer/Mapping/DoctorsMappingExtensions.cs
@@ -1,8 +1,22 @@
-﻿namespace MedicalClinicAPI.Mapping
-{
-    public class DoctorsMappingExtensions
-    {
+﻿using MedicalClinicAPI.DataLayer.Dtos;
+using MedicalClinicAPI.DataLayer.Mapping;
+using MedicalClinicAPI.Entities;
 
-        
+namespace MedicalClinicAPI.Mapping
+{
+    public static class DoctorsMappingExtensions
+    {
+        public static DoctorDto ToDoctorDto(this Doctor doctor)
+        {
+            if (doctor == null) return null;
+
+            var result = new DoctorDto();
+            result.Id = doctor.Id;
+            result.FullName = doctor.FirstName + " " + doctor.LastName;
+            result.Specialisation = doctor.Specialisation;
+            result.Pacients = doctor.Pacients.ToPacientDtos();
+            return result;
+        }
+
     }
 }
---
+++ MedicalClinicAPI/MedicalClinicAPI/DataLayer/Mapping/PacientsMappingExtensions.cs
@@ -0,0 +1,28 @@
+﻿using MedicalClinicAPI.DataLayer.Dtos;
+using MedicalClinicAPI.Entities;
+using System.Diagnostics;
+
+namespace MedicalClinicAPI.DataLayer.Mapping
+{
+    public static class PacientsMappingExtensions
+    {
+        public static List<PacientDto> ToPacientDtos(this List<Pacient> pacients)
+        {
+            var results = pacients.Select(e => e.ToPacientDto()).ToList();
+
+            return results;
+        }
+
+        public static PacientDto ToPacientDto(this Pacient pacient)
+        {
+            if (pacient == null) return null;
+
+            var result = new PacientDto();
+            result.FirstName = pacient.FirstName;
+            result.LastName = pacient.LastName;
+           
+
+            return result;
+        }
+    }
+}
